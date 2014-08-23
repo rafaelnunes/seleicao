ae2bc39469949019f10571a91873ac4309535dde
rafaelnunes <rafael.nunes@gmail.com>
2014-08-23 17:12:01 -0300

    Estilo de botão de voto

--- static/js/show_candidato.js
+++ static/js/show_candidato.js
@@ -14,4 +14,22 @@ $(document).ready(function() {
 			}
 		})
 	});
+
+	$('.btn-voto').click(function(event)){
+		event.preventDefault();
+		var voto = $(event.currentTarget);
+		$.ajax({
+			url: '/profile?id=' + $(obj).attr('id'),
+			type: 'GET',
+			dataType: 'json',
+			success: function(response){
+				$(obj).empty();
+				var tmpl = Mustache.render($('#tmpl_candidato_details').html(), response);
+				$(obj).append(tmpl);
+			},
+			error: function(error){
+				console.warn(error);
+			}
+		})
+	}
 });
