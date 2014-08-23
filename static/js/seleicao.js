d43888f1b9c5415c0bee5f129f21ffa3fe489e04
Caio Carrara <caiocarrara@gmail.com>
2014-08-23 12:59:29 -0300

    Adding wizard

---
+++ static/js/seleicao.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#rootwizard').bootstrapWizard({
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            $('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
+    }});
+});
