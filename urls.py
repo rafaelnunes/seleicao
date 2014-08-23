e7f6693ac571a9cc00952c9d3588e4333c53e543
Caio Carrara <caiocarrara@gmail.com>
2014-08-23 14:09:42 -0300

    Adding auth urls

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import webapp2
 
-from core.handlers import api
+from core.handlers import api, site
 from core.page_handlers import *
 from core.base import handle_404
 from core.template_filters import get_filters
@@ -12,7 +12,10 @@ routes = [('/', IndexPage),
 
 routes += [
 	webapp2.Route(r'/not-found', handler=IndexPage, handler_method='not_found', name='not_found'),
-	webapp2.Route('/api/load_transparencia', handler=api.APIHandler, handler_method='load_transparencia', name='load_transparencia')
+	webapp2.Route('/api/load_transparencia', handler=api.APIHandler, handler_method='load_transparencia', name='load_transparencia'),
+    webapp2.Route('/login', handler=site.SiteHandler, handler_method='login', name='login'),
+    webapp2.Route('/logout', handler=site.SiteHandler, handler_method='logout', name='logout'),
+    webapp2.Route('/register', handler=site.SiteHandler, handler_method='register', name='register')
 ]
 
 myconfig = {
