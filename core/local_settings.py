cb2ace04750a1153fe5905f138e2372a00b67859
Caio Carrara <caiocarrara@gmail.com>
2014-08-24 01:12:35 -0300

    Cleaning settings

--- core/local_settings.py
+++ core/local_settings.py
@@ -5,12 +5,9 @@ Created on Sept 26, 2013
 @author: Rafael Nunes
 '''
 
-HOST_NAME = 'http://projetootimizei.appspot.com'
+HOST_NAME = 'http://seleicao.appspot.com'
 
-APP_TITLE = 'Otimizei'
+APP_TITLE = 'Seleição'
 
 ALLOW_SOCIAL_LOGIN = False
 ALLOW_NEW_REGISTERS = True
-
-GOOGLE_CLIENT_ID = '349551308537.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = 'yC5xbGpfiZ3xpr-rxTOUM_I6'
