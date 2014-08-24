50a68630694357041d831474bd26fc3e50b43ec9
Caio Carrara <caiocarrara@gmail.com>
2014-08-24 01:20:36 -0300

    pep8

--- core/base.py
+++ core/base.py
@@ -16,16 +16,16 @@ import settings
 
 
 def jinja_factory(app):
-            """
-                Method for attaching aditional globals/filters to jinja rendering
-            """
-            j = jinja2.Jinja2(app)
-            j.environment.globals.update({
-                'uri_for': webapp2.uri_for,
-                'settings': settings,
-            })
+    """
+        Method for attaching aditional globals/filters to jinja rendering
+    """
+    j = jinja2.Jinja2(app)
+    j.environment.globals.update({
+        'uri_for': webapp2.uri_for,
+        'settings': settings,
+    })
 
-            return j
+    return j
 
 
 def user_required(handler):
@@ -33,6 +33,7 @@ def user_required(handler):
     Decorator that checks if there's a user associated with the current session.
     Will also fail if there's no session present.
     """
+
     def check_login(self, *args, **kwargs):
         auth = self.auth
         if not auth.get_user_by_session():
@@ -44,71 +45,72 @@ def user_required(handler):
 
 
 class BaseHandler(webapp2.RequestHandler):
-        @webapp2.cached_property
-        def jinja2(self):
-            j2 = jinja2.get_jinja2(factory=jinja_factory, app=self.app)
-            return j2
 
-        def render_str(self, filename, **template_args):
-            ctx = {'logged': self.logged}
-            ctx = {'settings': settings}
-            ctx.update(template_args)
+    @webapp2.cached_property
+    def jinja2(self):
+        j2 = jinja2.get_jinja2(factory=jinja_factory, app=self.app)
+        return j2
 
-            return self.jinja2.render_template(filename, **ctx)
-            
-        def render(self, filename, **template_args):
-            ctx = {'logged': self.logged}
-            ctx.update(template_args)
+    def render_str(self, filename, **template_args):
+        ctx = {'logged': self.logged}
+        ctx = {'settings': settings}
+        ctx.update(template_args)
 
-            self.response.write(self.jinja2.render_template(filename, **ctx))
+        return self.jinja2.render_template(filename, **ctx)
 
-        def render_json(self, obj):
-            self.response.headers['Content-Type'] = 'application/json' 
-            self.response.out.write(json.encode(obj))
+    def render(self, filename, **template_args):
+        ctx = {'logged': self.logged}
+        ctx.update(template_args)
 
-        def dispatch(self):
-            self.session_store = sessions.get_store(request=self.request)
+        self.response.write(self.jinja2.render_template(filename, **ctx))
 
-            try:
-                return  webapp2.RequestHandler.dispatch(self)
-            finally:
-                self.session_store.save_sessions(self.response)
+    def render_json(self, obj):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.encode(obj))
 
-        @webapp2.cached_property
-        def auth(self):
-            """Shortcut to access the auth instance as a property."""
-            return auth.get_auth()
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
 
-        @webapp2.cached_property
-        def user_info(self):
-            return self.auth.get_user_by_session()
+        try:
+            return webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
 
-        @webapp2.cached_property
-        def session(self):
-            return self.session_store.get_session(backend="datastore")
+    @webapp2.cached_property
+    def auth(self):
+        """Shortcut to access the auth instance as a property."""
+        return auth.get_auth()
 
-        @webapp2.cached_property
-        def logged(self):
-            u = self.user_info
-            return self.user_model.get_by_id(u['user_id']) if u else None
+    @webapp2.cached_property
+    def user_info(self):
+        return self.auth.get_user_by_session()
 
-        @webapp2.cached_property
-        def user_model(self):
-            return self.auth.store.user_model
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session(backend="datastore")
 
-        def handle_exception(self, exception, debug):
-		    # Log the error
-            log.exception(exception)
+    @webapp2.cached_property
+    def logged(self):
+        u = self.user_info
+        return self.user_model.get_by_id(u['user_id']) if u else None
 
-            if isinstance(exception, webapp2.exc.HTTPNotFound):
-                self.render('app/404.html')
+    @webapp2.cached_property
+    def user_model(self):
+        return self.auth.store.user_model
 
-        def get_object_or_404(self, cls, object_id, parent=None):
-            instance = cls.get_by_id(object_id, parent=parent)
-            if not instance:
-                return self.abort(404)
+    def handle_exception(self, exception, debug):
+                # Log the error
+        log.exception(exception)
 
-            return instance
+        if isinstance(exception, webapp2.exc.HTTPNotFound):
+            self.render('app/404.html')
+
+    def get_object_or_404(self, cls, object_id, parent=None):
+        instance = cls.get_by_id(object_id, parent=parent)
+        if not instance:
+            return self.abort(404)
+
+        return instance
 
 
 def handle_404(request, response, exception):
@@ -116,20 +118,20 @@ def handle_404(request, response, exception):
 
 
 class BaseModel(ndb.Model):
-  
+
     def to_dict(self, ignore=[]):
-	as_json = {}
-	for p in self._properties:
-	   if p in ignore:
-	     continue
-	  
-	   if type(getattr(self, p)) == ndb.key.Key:
-	     as_json[p] = getattr(self, p).id()
-	   else:
-	     as_json[p] = unicode(getattr(self, p))
-	
-	if self.key:
-	  as_json['id'] = self.key.id()
+        as_json = {}
+        for p in self._properties:
+            if p in ignore:
+                continue
+
+            if type(getattr(self, p)) == ndb.key.Key:
+                as_json[p] = getattr(self, p).id()
+            else:
+                as_json[p] = unicode(getattr(self, p))
+
+        if self.key:
+            as_json['id'] = self.key.id()
         return as_json
 
     def put_async(self):
