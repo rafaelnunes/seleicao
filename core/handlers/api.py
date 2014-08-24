31556a803c0d084411811420d569ffc0b4000532
Caio Carrara <caiocarrara@gmail.com>
2014-08-24 01:03:37 -0300

    pep8

--- core/handlers/api.py
+++ core/handlers/api.py
@@ -11,130 +11,139 @@ from core import settings
 from core.base import BaseHandler, user_required
 from core.models import Estado, Partido, Cargo, Candidato
 
+
 class APIHandler(BaseHandler):
 
-	def load_dados_transparencia(self):
-		deferred.defer(load_estados, _queue='loadTransparencia')
-		deferred.defer(load_partidos, _queue='loadTransparencia')
-		deferred.defer(load_cargos, _queue='loadTransparencia')
+    def load_dados_transparencia(self):
+        deferred.defer(load_estados, _queue='loadTransparencia')
+        deferred.defer(load_partidos, _queue='loadTransparencia')
+        deferred.defer(load_cargos, _queue='loadTransparencia')
 
-	def load_candidatos_transparencia(self):
-		deferred.defer(load_candidatos, _queue='loadTransparencia')
+    def load_candidatos_transparencia(self):
+        deferred.defer(load_candidatos, _queue='loadTransparencia')
 
 
 def load_estados():
-	ndb.delete_multi(
-    	Estado.query().fetch(keys_only=True)
-	)
+    ndb.delete_multi(
+        Estado.query().fetch(keys_only=True)
+    )
 
-	try:
-		opener = urllib2.build_opener()
-		opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
-		result = opener.open(settings.uri('estados'))
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                             ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+        result = opener.open(settings.uri('estados'))
 
-		estados = json.decode(result.read())
-		for estado in estados:
-			Estado(estado_id=int(estado['estadoId']), nome=estado['nome'], sigla=estado['sigla']).put()
-	except urllib2.URLError, e:
-		print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-		pass
+        estados = json.decode(result.read())
+        for estado in estados:
+            Estado(estado_id=int(estado['estadoId']), nome=estado[
+                   'nome'], sigla=estado['sigla']).put()
+    except urllib2.URLError, e:
+        print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+        pass
 
 
 def load_partidos():
-	ndb.delete_multi(
-    	Partido.query().fetch(keys_only=True)
-	)
-	try:
-		opener = urllib2.build_opener()
-		opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
-		result = opener.open(settings.uri('partidos'))
+    ndb.delete_multi(
+        Partido.query().fetch(keys_only=True)
+    )
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                             ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+        result = opener.open(settings.uri('partidos'))
 
-		partidos = json.decode(result.read())
-		for partido in partidos:
-			Partido(partido_id=int(partido['partidoId']), sigla=partido['sigla']).put()
-	except urllib2.URLError, e:
-		print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-		pass
+        partidos = json.decode(result.read())
+        for partido in partidos:
+            Partido(
+                partido_id=int(partido['partidoId']), sigla=partido['sigla']).put()
+    except urllib2.URLError, e:
+        print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+        pass
 
 
 def load_cargos():
-	ndb.delete_multi(
-    	Cargo.query().fetch(keys_only=True)
-	)
+    ndb.delete_multi(
+        Cargo.query().fetch(keys_only=True)
+    )
 
-	try:
-		opener = urllib2.build_opener()
-		opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
-		result = opener.open(settings.uri('cargos'))
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                             ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+        result = opener.open(settings.uri('cargos'))
 
-		cargos = json.decode(result.read())
-		for cargo in cargos:
-			Cargo(cargo_id=int(cargo['cargoId']), nome=cargo['nome']).put()
-	except urllib2.URLError, e:
-		print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-		pass
+        cargos = json.decode(result.read())
+        for cargo in cargos:
+            Cargo(cargo_id=int(cargo['cargoId']), nome=cargo['nome']).put()
+    except urllib2.URLError, e:
+        print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+        pass
 
 
 def load_candidatos():
-	ndb.delete_multi(
-    	Candidato.query().fetch(keys_only=True)
-	)
+    ndb.delete_multi(
+        Candidato.query().fetch(keys_only=True)
+    )
 
-	try:
-		opener = urllib2.build_opener()
-		opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                             ('Content-Type', 'application/json'), ('Accept', 'application/json')]
 
-		uri = settings.uri('candidatos_by_uf_cargo')
+        uri = settings.uri('candidatos_by_uf_cargo')
 
-		estados = Estado.query().fetch()
-		for estado in estados:
-			_load_candidato_by_uf(estado)
-		
-	except urllib2.URLError, e:
-		print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-		pass
+        estados = Estado.query().fetch()
+        for estado in estados:
+            _load_candidato_by_uf(estado)
+
+    except urllib2.URLError, e:
+        print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+        pass
 
 
 def _load_candidato_by_uf(estado):
-	cargos = Cargo.query().fetch()
-	for cargo in cargos:
-		try:
-			opener = urllib2.build_opener()
-			opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+    cargos = Cargo.query().fetch()
+    for cargo in cargos:
+        try:
+            opener = urllib2.build_opener()
+            opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                                 ('Content-Type', 'application/json'), ('Accept', 'application/json')]
 
-			uri = settings.uri('candidatos_by_uf_cargo').format(uf=estado.sigla, cargo=cargo.cargo_id)
-			result = opener.open(uri)
+            uri = settings.uri('candidatos_by_uf_cargo').format(
+                uf=estado.sigla, cargo=cargo.cargo_id)
+            result = opener.open(uri)
 
-			candidatos = json.decode(result.read())
-			for candidato in candidatos:
-				_put_candidate(candidato)
+            candidatos = json.decode(result.read())
+            for candidato in candidatos:
+                _put_candidate(candidato)
 
-		except urllib2.URLError, e:
-			print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-			pass
+        except urllib2.URLError, e:
+            print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+            pass
 
 
 def _put_candidate(candidato):
-	try:
-		opener = urllib2.build_opener()
-		opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN), ('Content-Type', 'application/json'), ('Accept', 'application/json')]
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('App-Token', settings.TRANSPARENCIA_TOKEN),
+                             ('Content-Type', 'application/json'), ('Accept', 'application/json')]
 
-		uri = settings.uri('candidato_stats').format(candidato_id=candidato['id'])
-		result = opener.open(uri)
+        uri = settings.uri('candidato_stats').format(
+            candidato_id=candidato['id'])
+        result = opener.open(uri)
 
-		candidato_stats = json.decode(result.read())
-		if candidato_stats:
-			candidato_stats = candidato_stats[0]
-		else:
-			candidato_stats = {}
+        candidato_stats = json.decode(result.read())
+        if candidato_stats:
+            candidato_stats = candidato_stats[0]
+        else:
+            candidato_stats = {}
 
-		Candidato(candidato_id=int(candidato['id']), instrucao=candidato['instrucao'], reeleicao=candidato['reeleicao'], cargo=candidato['cargo'],
-			estado=candidato['estado'], nome=candidato['nome'], foto=candidato['foto'],
-			faltas_plenario=float(candidato_stats.get('faltas_plen',0)), 
-			faltas_comissoes=float(candidato_stats.get('faltas_com', 0))).put()
-	except urllib2.URLError, e:
-		print '>>>>>>>>>>>>>>>>>>>> %s' %str(e)
-		pass
-
-
-
+        Candidato(candidato_id=int(candidato['id']), instrucao=candidato['instrucao'], reeleicao=candidato['reeleicao'], cargo=candidato['cargo'],
+                  estado=candidato['estado'], nome=candidato[
+                      'nome'], foto=candidato['foto'],
+                  faltas_plenario=float(candidato_stats.get('faltas_plen', 0)),
+                  faltas_comissoes=float(candidato_stats.get('faltas_com', 0))).put()
+    except urllib2.URLError, e:
+        print '>>>>>>>>>>>>>>>>>>>> %s' % str(e)
+        pass
--- core/handlers/votados.py
+++ core/handlers/votados.py
@@ -4,48 +4,45 @@ Created on Sept 26, 2013
 
 @author: Rafael Nunes
 '''
-from google.appengine.ext import ndb
-
 from core.base import BaseHandler
 from core.models import Voto, Candidato
 
+
 class VotadosHandler(BaseHandler):
+
     def get(self):
-		usuario = self.logged
-		
-		votaria_ids = Voto.query(Voto.usuario == usuario.email_address, Voto.pontos == 1).fetch()
-		
-		nao_votaria_ids = Voto.query(Voto.usuario == usuario.email_address, Voto.pontos == -1).fetch()
-		
-		talvez_votaria_ids = Voto.query(Voto.usuario == usuario.email_address, Voto.pontos == 0).fetch()
-		
-		votaria = [Candidato.query(Candidato.candidato_id == int(k.candidato_id)).fetch()[0] for k in votaria_ids]
-		
-		nao_votaria = [Candidato.query(Candidato.candidato_id == int(k.candidato_id)).fetch()[0] for k in nao_votaria_ids]
+        usuario = self.logged
 
-		talvez_votaria = [Candidato.query(Candidato.candidato_id == int(k.candidato_id)).fetch()[0] for k in talvez_votaria_ids]
+        votaria_ids = Voto.query(
+            Voto.usuario == usuario.email_address, Voto.pontos == 1).fetch()
 
-		#votaria = Candidato.query(Candidato.estado == estado, 				
-					# ndb.AND(cargo!="",Candidato.cargo == cargo), 
-					# ndb.AND(reeleicao!="",Candidato.reeleicao == reeleicao),
-					# ndb.AND(faltas_sessoes!="",Candidato.faltas_sessoes <= faltas_sessoes),
-					# ndb.AND(faltas_comissoes!="",Candidato.faltas_comissoes <= faltas_comissoes),
-					# ndb.AND(instrucao!="", grau[Candidato.instrucao] >= instrucao),
-					# ndb.AND(processos!="", Candidato.processos == processos),				
-		#			).fetch()
-		
-	        return self.render('show_votados.html', votaria=votaria, nao_votaria=nao_votaria, talvez_votaria=talvez_votaria)
+        nao_votaria_ids = Voto.query(
+            Voto.usuario == usuario.email_address, Voto.pontos == -1).fetch()
+
+        talvez_votaria_ids = Voto.query(
+            Voto.usuario == usuario.email_address, Voto.pontos == 0).fetch()
+
+        votaria = [Candidato.query(
+            Candidato.candidato_id == int(k.candidato_id)).fetch()[0] for k in votaria_ids]
+
+        nao_votaria = [Candidato.query(Candidato.candidato_id == int(
+            k.candidato_id)).fetch()[0] for k in nao_votaria_ids]
+
+        talvez_votaria = [Candidato.query(Candidato.candidato_id == int(
+            k.candidato_id)).fetch()[0] for k in talvez_votaria_ids]
+
+        return self.render('show_votados.html', votaria=votaria, nao_votaria=nao_votaria, talvez_votaria=talvez_votaria)
 
 
 class VotoHandler(BaseHandler):
-	def registrar(self):
-		voto = Voto()
-		
-		voto.usuario = self.logged.email if self.logged else None
-		voto.candidato_id = self.request.get('candidato')
-		voto.pontos = int(self.request.get('voto'))
-		
-		voto.put()		
-			
-		return self.render_json({'success': True })
 
+    def registrar(self):
+        voto = Voto()
+
+        voto.usuario = self.logged.email if self.logged else None
+        voto.candidato_id = self.request.get('candidato')
+        voto.pontos = int(self.request.get('voto'))
+
+        voto.put()
+
+        return self.render_json({'success': True})
